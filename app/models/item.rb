800cfaf9a7f4ef5355eb09c1b2101c531871ef20
sparsh <sparshsamir1993@gmail.com>
2016-05-15 16:28:46 +0530

    edits

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
 	belongs_to :restaurant
-	belongs_to :order
+	
 	validates :name,  :presence => true
   	validates :price, :presence => true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,6 @@
 class Order < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :restaurant, foreign_key: 'restaurant_id'
-	has_many :items
 	serialize :order, Hash
 
 end
