becd80123f7d63b69d5fe52b280f48d07e2f01ec
sparsh <sparshsamir1993@gmail.com>
2016-05-25 13:41:08 +0530

    Update button working

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   def index
   		@restaurant = Restaurant.find(params[:restaurant_id])
-	    @items = @restaurant.items.search(params[:search]).paginate(page: params[:page], per_page: 40)
+	    @items = @restaurant.items.search(params[:search]).paginate(page: params[:page], per_page: 60)
 	    
 	    @user = current_user
 	    @restaurant_user = @restaurant.users.new
