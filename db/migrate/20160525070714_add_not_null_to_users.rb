9277453dfe3548ea24622b012985bc2ee7757f26
sparsh <sparshsamir1993@gmail.com>
2016-05-25 12:42:10 +0530

    Update button working

---
+++ db/migrate/20160525070714_add_not_null_to_users.rb
@@ -0,0 +1,5 @@
+class AddNotNullToUsers < ActiveRecord::Migration
+  def change
+  	change_column_null :users, :email, true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160519111532) do
+ActiveRecord::Schema.define(version: 20160525070714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 20160519111532) do
     t.datetime "oauth_expires_at"
     t.string   "contact"
     t.string   "address"
-    t.string   "email",                  default: "", null: false
+    t.string   "email",                  default: ""
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
