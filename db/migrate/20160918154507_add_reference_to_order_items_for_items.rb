40806197a95084fb690feacd9c02135e9f73dd96
sparsh <sparshsamir1993@gmail.com>
2016-10-12 13:24:00 +0530

    assets precompile

--- db/migrate/20160918154507_add_reference_to_order_items_for_items.rb
+++ db/migrate/20160918154507_add_reference_to_order_items_for_items.rb
@@ -1,6 +1,5 @@
 class AddReferenceToOrderItemsForItems < ActiveRecord::Migration
   def change
       add_reference :order_items, :item, foreign_key: true
-      remove_column :orders, :item_id
   end
 end
