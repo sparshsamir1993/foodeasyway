a72033a35982738c7c20e9a8cedce156498ecbed
Sparsh <sparshsamir1993@gmail.com>
2018-03-24 16:03:18 +0530

    restaurant show page

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ActiveRecord::Base
     "Soup": 0,
 		"Starters": 1,
 		"Breads":2,
-		"Rice/Biriyani":3,
+		"Rice or Biriyani":3,
 		"Main course": 4,
 		"Dessert": 5,
   }
@@ -15,22 +15,11 @@ class Item < ActiveRecord::Base
 
 	scope :search, lambda { |query|
 	   return nil  if query.blank?
-	 # Searches the students table on the 'first_name' and 'last_name' columns.
-	 # Matches using LIKE, automatically appends '%' to each term.
-	 # LIKE is case INsensitive with MySQL, however it is case
-	 # sensitive with PostGreSQL. To make it work in both worlds,
-	 # we downcase everything.
-	 # condition query, parse into individual keywords
 	 terms = query.downcase.split(/\s+/)
 
-	 # replace "*" with "%" for wildcard searches,
-	 # append '%', remove duplicate '%'s
 	 terms = terms.map { |e|
 	   (e.gsub('*', '%').prepend('%') + '%').gsub(/%+/, '%')
 	 }
-	 # configure number of OR conditions for provision
-	 # of interpolation arguments. Adjust this if you
-	 # change the number of OR conditions.
 	 num_or_conds = 1
 	 where(
 	   terms.map { |term|
