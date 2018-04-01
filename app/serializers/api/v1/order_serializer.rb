4c12924b40b6c7ca1792fa63c3bae8baa35e5d8b
Sparsh <sparshsamir1993@gmail.com>
2018-04-01 15:09:16 +0530

    serializer changes

--- app/serializers/api/v1/order_serializer.rb
+++ app/serializers/api/v1/order_serializer.rb
@@ -1,11 +1,12 @@
 class Api::V1::OrderSerializer < ActiveModel::Serializer
-    attributes :id, :order_address, :order_items, :order_address, :order_restaurants, :restaurants
+
     belongs_to :user
 	has_many :order_restaurants
 	has_many :restaurants, through: :order_restaurants
 	has_one :order_address
 	has_many :order_items
 	has_many :items, through: :order_items
+	attributes :id, :order_address, :order_items, :order_address, :order_restaurants
     def total_bill
     end
 
