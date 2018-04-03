794b4054c47beb6b8ecd00d53b4c2e669e4f6752
Sparsh <sparshsamir1993@gmail.com>
2018-04-04 01:40:26 +0530

    restaurant owner order controller fix

--- app/controllers/api/v1/restaurant_interface/orders_controller.rb
+++ app/controllers/api/v1/restaurant_interface/orders_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::RestaurantInterface::OrdersController < Api::V1::RestaurantInterf
 	
 
     def index
-        @restaurant_id = current_user.restaurant_owners.first.restaurant.id
+        @restaurant_id = User.find(params[:user_id]).restaurant_owners.first.restaurant.id
         @orders = OrderRestaurant.where(restaurant_id: @restaurant_id).order('created_at DESC')
         render json: @orders,
            each_serializer: Api::V1::RestaurantInterface::OrdersRestaurantSerializer
